ffc0c48a7c6ccaedb28060ec58149080516d54b3
MariaBelenGallardoPedretti <belengallardop@gmail.com>
2024-06-27 08:09:11 +0000

    Generador de dominios

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function generadorDominio() {
+    let pronombres = ["El", "La"];
+    let sustantivos = ["Edificio", "Cabaña"];
+    let adjetivos = ["genial", "impresionante", "espectacular"];
+    let dominios = [];
+
+    pronombres.forEach(pronombre => {
+      sustantivos.forEach(sustantivo => {
+        adjetivos.forEach(adjetivo => {
+          let dominio = pronombre + sustantivo + adjetivo + ".com";
+          dominios.push(dominio);
+        });
+      });
+    });
+
+    return dominios;
+  }
+
+  let pruebaDominios = generadorDominio();
+  console.log(pruebaDominios);
+  document.getElementById("dominios").innerText = generadorDominio();
 };
